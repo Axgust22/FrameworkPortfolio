43f8e8aba50f6e3c10aa7b3a412807e3dfaf5079
August <macbookpro@August22.local>
2025-02-27 13:18:42 +0700

    up

--- src/app/page.js
+++ src/app/page.js
@@ -153,7 +153,7 @@ export default function Home() {
               </h2>
               <div id="StudentInformation">
                 <img
-                  src="Image/studentiformation.jpg"
+                  src="Image/studentin`formation.jpg"
                   alt="Student Information System"
                   className="w-full h-auto object-cover rounded-lg shadow-lg"
                 />
